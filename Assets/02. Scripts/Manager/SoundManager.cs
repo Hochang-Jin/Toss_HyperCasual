d1e23d2a43ca8915b5c690ee6cd24834c6905fbe
HochangJin <hochang0115@gmail.com>
2025-07-14 16:48:41 +0900

    [ADD] Sound manager

---
+++ Assets/02. Scripts/Manager/SoundManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource backgroundSource;
+    public AudioSource timerSource;
+    
+    public AudioClip backgroundMusic;
+    public AudioClip timerSound;
+    public AudioClip effectSound;
+
+}
