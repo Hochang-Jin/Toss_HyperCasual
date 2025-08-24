031478dc6a01fd05c5124896c2dc9a1e71b1cbea
HochangJin <hochang0115@gmail.com>
2025-08-24 20:47:54 +0900

    .

--- Assets/02. Scripts/Manager/UIOrientationManager.cs
+++ Assets/02. Scripts/Manager/UIOrientationManager.cs
@@ -26,14 +26,14 @@ public class UIOrientationManager : MonoBehaviour
     {
         if (Screen.width > Screen.height) // 가로
         {
-            Debug.Log("가로");
             UIManager.Instance.SetUI(1);
+            Screen.SetResolution(1920,1080,false);
             GameManager.Instance.AfterUIChanged();
         }
         else
         {
-            Debug.Log("세로");
             UIManager.Instance.SetUI(0);
+            Screen.SetResolution(1200,1600,false);
             GameManager.Instance.AfterUIChanged();
         }
     }
